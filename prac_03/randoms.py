773b62325c2f2b659266b1e72a314d227e9e29a5
Koen <Kjamesm04@outlook.com>
2024-10-03 12:42:43 +1000

    Add randoms to practical 3

---
+++ prac_03/randoms.py
@@ -0,0 +1,11 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+ #1. I saw a random number from 5 to 20. The lowest number was 6, highest number was 20
+
+ #2. I saw a random number, it was either a 3, 5, 7 or 9. You can't get a 4.
+
+ #3. I saw a random number with many decimal places that could vary from 2.5 to 5.5
