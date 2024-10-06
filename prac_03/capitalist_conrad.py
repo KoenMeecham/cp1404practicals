a39a60b9d0c04900a536e4c96ea16659d48a0f33
Koen <Kjamesm04@outlook.com>
2024-10-06 10:18:11 +1000

    add capitalist conrad in practical 3

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -14,12 +14,17 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
 MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
+FILENAME = "stock_output.txt"
+
+out_file = open(FILENAME, "w")
 
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+day = 0
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
+    day += 1
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
     if random.randint(1, 2) == 1:
@@ -32,4 +37,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {day} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
