b7b64d1d77f09c7b6176b564bba78d5aa46e80f6
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-10-31 13:25:57 +1000

    Complete programming_language in prac 6

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,8 +1,18 @@
+"""
+CP1402 prac 6
+programming language
+expected: 10mins
+actual:
+"""
+
 class ProgrammingLanguage:
+    """Represent a car object"""
     def __init__(self, typing="", reflection="", date=0):
+        """Construct a programming language instance"""
         self.typing = typing
         self.reflection = reflection
         self.date = date
 
     def is_dynamic(self):
+        """Determine if this is a dynamic programming language"""
         return self.typing == "dynamic"
