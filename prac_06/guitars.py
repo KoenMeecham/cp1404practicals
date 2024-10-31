166d87e1e0eb1b723bde8f265ccc0706b03cbc26
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-10-31 14:42:28 +1000

    Complete guitars in prac 6

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -26,9 +26,7 @@ def main():
     if guitars != "":
         print("My guitars!")
         for i, guitar in enumerate(guitars):
-            vintage_string = ""
-            if guitar.is_vintage():
-                vintage_string = " vintage"
+            vintage_string = " vintage" if guitar.is_vintage() else ""
             print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
     else:
         print("No guitars!, go get some")
