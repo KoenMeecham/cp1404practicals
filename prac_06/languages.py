8932ac7152a63cd5bef2ccd2d9a54c33745aa321
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-10-31 13:32:59 +1000

    Refactor languages in prac 6

---
+++ prac_06/languages.py
@@ -0,0 +1,18 @@
+"""
+CP1402
+languages
+expected: 10mins
+actual
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+
+main()
