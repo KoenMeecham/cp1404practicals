cc298a3f1a4536c54feed04256225305ccba9cef
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-10-31 13:53:08 +1000

    Add guitar to prac 6

---
+++ prac_06/guitar.py
@@ -0,0 +1,28 @@
+"""
+CP1402
+guitar
+expected: 30mins
+actual
+"""
+CURRENT_YEAR = 2024
+VINTAGE_AGE = 50
+class Guitar:
+    """Represents a guitar object"""
+    def __init__(self, name, year, cost):
+        """Construct a guitar instance"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of the guitar"""
+        return f'{self.name} ({self.year}) :  ${self.cost}'
+
+    def get_age(self):
+        """Return the age of the guitar"""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Return true if the guitar is vintage"""
+        return self.get_age() >= VINTAGE_AGE
+
