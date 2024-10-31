491a27a5a8067cf4ce4c018c8808ddf63c902f75
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-10-31 14:14:34 +1000

    Complete guitar test in parc 6

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -4,4 +4,20 @@ guitar test
 expected: 30mins
 actual:
 """
+from prac_06.guitar import Guitar
+def run_tests():
+    """Tests for Guitar"""
+    name = "Gibson L-5 CES"
+    year = 1922
+    cost = 16035.40
 
+    guitar = Guitar(name, year, cost)
+    other = Guitar("Another Guitar", 2012, 1512.9)
+    print(f"{guitar.name} get_age() - Expected {95}. Got {guitar.get_age()}")
+    print(f"{other.name} get_age() - Expected {5}. Got {other.get_age()}")
+    print()
+    print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
+    print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
+
+if __name__ == "__main__":
+    run_tests()
