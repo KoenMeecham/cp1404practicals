33a561eb957f1cb1b36976814ded6cc4fd13eb48
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-11-03 14:56:09 +1000

    Refactor guitar main into two other functions, display_guitars and append_guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -20,9 +20,12 @@ def main():
         guitars.append(add_guitar)
         name = input("Name: ")
 
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Amazing guitar", 2020, 20000))
-    #Call is_vintage function
+    append_guitars(guitars)
+    display_guitars(guitars)
+
+
+def display_guitars(guitars):
+    # Call is_vintage function
     if guitars != "":
         print("My guitars!")
         for i, guitar in enumerate(guitars):
@@ -30,5 +33,11 @@ def main():
             print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
     else:
         print("No guitars!, go get some")
+
+
+def append_guitars(guitars):
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Amazing guitar", 2020, 20000))
+
 
 main()
