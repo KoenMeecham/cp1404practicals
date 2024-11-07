9ed57fc4a9ef504c02fa37a059c6829a53fd111f
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-11-07 13:36:57 +1000

    Add pointer arithmetic instance to programming_langauge

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -1,22 +1,25 @@
 """
 CP1404/CP5632 Practical - Suggested Solution
 Programming Language class with tests.
+expected: 15mins
+actual:
 """
 
 
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, Pointer arithmetic={self.pointer_arithmetic}"
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
@@ -25,10 +28,10 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    english = ProgrammingLanguage("English", "Dynamic", False, 1000)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, False)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, False)
+    english = ProgrammingLanguage("English", "Dynamic", False, 1000, False)
     languages = [ruby, python, visual_basic, english]
     print(python)
 
