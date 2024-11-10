a123d55b06afe02f0a0512f70682f5d08ab8a806
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-11-10 14:46:13 +1000

    Add display guitars function to myguitars

---
+++ prac_07/myguitars.py
@@ -0,0 +1,39 @@
+"""
+CP1404
+myguitars
+expected: 20mins
+actual:
+"""
+
+import csv
+from prac_07.guitar import Guitar
+from collections import namedtuple
+
+def main():
+    """Program for displaying guitars"""
+    guitars = load_guitars()
+    display_guitars(guitars)
+
+
+def load_guitars():
+    """Load guitars"""
+    guitars = []
+    with open ("guitars.csv", "r") as in_file:
+        csv_reader = csv.reader(in_file)
+        for row in csv_reader:
+            guitar = Guitar(row[0], int(row[1]), float(row[2]))
+            guitars.append(guitar)
+    return guitars
+
+def display_guitars(guitars):
+    """Display guitars sorted by year"""
+    guitars.sort()
+    for guitar in guitars:
+        print(guitar)
+
+
+def save_guitars():
+    """Save the guitars to the csv"""
+    print("Saving guitars")
+
+main()
