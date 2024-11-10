9d1f3f3f1a54007291d7633c201cb23f9c9d46f5
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-11-10 15:08:18 +1000

    Complete myguitars in prac 7

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,7 +2,7 @@
 CP1404
 myguitars
 expected: 20mins
-actual:
+actual: 22mins
 """
 
 import csv
@@ -12,7 +12,9 @@ from prac_07.guitar import Guitar
 def main():
     """Program for displaying guitars"""
     guitars = load_guitars()
+    add_guitars(guitars)
     display_guitars(guitars)
+    save_guitars(guitars)
 
 
 def load_guitars():
@@ -31,14 +33,24 @@ def display_guitars(guitars):
     for guitar in guitars:
         print(guitar)
 
+def add_guitars(guitars):
+    """Add guitars to the list from user input."""
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        add_guitar = Guitar(name, year, cost)
+        guitars.append(add_guitar)
+        name = input("Name: ")
+    return guitars
 
 def save_guitars(guitars):
     """Save the guitars to the csv"""
     print("Saving guitars")
-    with open ("guitars.csv", "w") as outfile:
-        csv_writer = csv.writer(outfile)
-        for row in guitars:
-            csv_writer.writerow(row)
+    with open ("guitars.csv", "w", newline='') as out_file:
+        csv_writer = csv.writer(out_file)
+        for guitar in guitars:
+            csv_writer.writerow([guitar.name, guitar.year, guitar.cost])
 
 if __name__ == "__main__":
     main()
