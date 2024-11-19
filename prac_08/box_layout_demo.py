8fd8f581f50abd195269ca4f47cac7c57d3ad5e1
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-11-19 12:56:32 +1000

    Add docstrings to box_layout_demo.py

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,18 +1,28 @@
+"""
+CP1404
+Box Layout Demo
+estimated: 5mins
+actual: 6mins
+"""
 from kivy.app import App
 from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Kivy Box App"""
     def build(self):
+        """Build Kivy App"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Greet the user"""
         print("Hello")
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Clear user's name"""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
