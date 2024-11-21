fdc973a9b9c04e70c2ee6d47ee1cab49ceb73663
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-11-21 13:04:46 +1000

    Add taxi_simulator.py

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,16 @@
+"""
+CP1404
+Taxi sim
+estimated: 25mins
+actual:
+"""
+
+from prac_09.car import Car
+from prac_09.taxi import Taxi
+from prac_09.sliver_service_taxi import SliverServiceTaxi
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
