19f3862d7fc582509d1088301f726519e9b433fd
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-11-21 11:38:02 +1000

    Refactor price_per_km to a constant

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -9,7 +9,7 @@ from prac_09.taxi import Taxi
 
 def main():
     """Test taxi"""
-    taxi_test = Taxi("Prius 1", 100, 1.23)
+    taxi_test = Taxi("Prius 1", 100)
     taxi_test.drive(40)
     print(taxi_test, taxi_test.get_fare())
     taxi_test.start_fare()
