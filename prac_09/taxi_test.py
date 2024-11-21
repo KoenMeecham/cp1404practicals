3231f1697e9e3336fc1454b7549d921edce7775e
KoenMeecham <koen.meecham@my.jcu.edu.au>
2024-11-21 11:32:59 +1000

    Add taxi_test.py

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,20 @@
+"""
+CP1404
+Taxi test
+estimated: 8mins
+actual:
+"""
+
+from prac_09.taxi import Taxi
+
+def main():
+    """Test taxi"""
+    taxi_test = Taxi("Prius 1", 100, 1.23)
+    taxi_test.drive(40)
+    print(taxi_test)
+    taxi_test.start_fare()
+    taxi_test.drive(100)
+    print(taxi_test)
+
+
+main()
